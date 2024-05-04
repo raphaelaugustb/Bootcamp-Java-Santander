4ddd24db73ffd9a36bed93224610a5ba684547c3
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-03 22:54:39 -0300

    Novo modulo operadores em java

---
+++ Java-Syntax/TiposVariaveis/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Constante
+        final String NOME = "Raphael";
+
+        // Valores Decimais
+        double pi = 3.14;
+
+        // Valores Inteiros
+        int salario = 2400;
+
+        // Strings
+        String sobrenome = "Augusto";
+    }
+}
