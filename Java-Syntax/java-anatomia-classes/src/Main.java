e1c90a7bb1b96df0d65dfd6a8b29f8c42ea4bb8b
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-03 21:53:14 -0300

    Novo modulo tipos e variaveis

--- Java-Syntax/java-anatomia-classes/src/Main.java
+++ Java-Syntax/java-anatomia-classes/src/Main.java
@@ -1,5 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
- 
+    public static void main (String[] args){
+        String ano_fabricacao = "teste";
+        // Errado: String ano fabricacao = "teste";
+        int valorCasa = 1;
+        //int errado valorcasa = 1
+
+        //Variavel maiscula nao muda valor
+        String BR = "Brasil";
+        String firstName = "Raphael";
+        String secondName = "Augusto";
+        String nomeCompleto = nomeCompleto(firstName, secondName);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String firstName, String secondName){
+    return firstName.concat(" ").concat(secondName);
+    }
+
 }
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
--- Java-Syntax/java-anatomia-classes/src/edu/gleyson/primeirasemana/PrimeiraSemana.java
+++ Java-Syntax/java-anatomia-classes/src/edu/gleyson/primeirasemana/PrimeiraSemana.java
@@ -1,24 +1,41 @@
+//Criacao de pacotes
 package edu.gleyson.primeirasemana;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
+
 public class PrimeiraSemana {
     public static void main (String[] args){
-        String ano_fabricacao = "teste";
-        // Errado: String ano fabricacao = "teste";
-        int valorCasa = 1;
-        //int errado valorcasa = 1
 
-        //Variavel maiscula nao muda valor
-        String BR = "Brasil";
-        String firstName = "Raphael";
-        String secondName = "Augusto";
-        String nomeCompleto = nomeCompleto(firstName, secondName);
-        System.out.println(nomeCompleto);
+        System.out.println("Digite o nome do aluno");
+        Scanner getAlunoNome = new Scanner(System.in);
+
+
+        System.out.println("Digite o valor da nota do aluno");
+        Scanner getAlunoMedia = new Scanner(System.in);
+
+        String nomeAluno = getAlunoNome.nextLine();
+        int notaAluno = getAlunoMedia.nextInt();
+            getAlunoNome.close();
+            getAlunoMedia.close();
+
+        System.out.println(mediaAluno(notaAluno, nomeAluno));
     }
 
-    public static String nomeCompleto (String firstName, String secondName){
-    return firstName.concat(" ").concat(secondName);
+    public static String mediaAluno (int notaAluno, String nomeAluno){
+        String alunoStatus;
+
+        if(notaAluno < 6){
+            alunoStatus = nomeAluno + " foi Reprovado." ;
+        } else if (notaAluno == 6) {
+            alunoStatus = "Aluno " + nomeAluno + " está na média." ;
+        } else{
+            alunoStatus =  "Aluno " + nomeAluno +  "foi Aprovado.";
+        }
+     return alunoStatus;
     }
+
+
+
 
 }
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
