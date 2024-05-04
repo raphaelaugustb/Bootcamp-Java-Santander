ec3c3742df1183d24c3e75afd33a81d95109c804
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-03 21:44:36 -0300

    Anatomia de classes

---
+++ Java-Syntax/java-anatomia-classes/src/edu/gleyson/primeirasemana/PrimeiraSemana.java
@@ -0,0 +1,24 @@
+package edu.gleyson.primeirasemana;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+public class PrimeiraSemana {
+    public static void main (String[] args){
+        String ano_fabricacao = "teste";
+        // Errado: String ano fabricacao = "teste";
+        int valorCasa = 1;
+        //int errado valorcasa = 1
+
+        //Variavel maiscula nao muda valor
+        String BR = "Brasil";
+        String firstName = "Raphael";
+        String secondName = "Augusto";
+        String nomeCompleto = nomeCompleto(firstName, secondName);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String firstName, String secondName){
+    return firstName.concat(" ").concat(secondName);
+    }
+
+}
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
