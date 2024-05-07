b3c4361f939623a2750b045158a70e1eca047ba0
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-06 22:30:52 -0300

    Novo modulo operadores em java finalizado

--- Java-Syntax/Operadores/src/Operadores.java
+++ Java-Syntax/Operadores/src/Operadores.java
@@ -2,7 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Operadores {
     public static void main(String[] args) {
-        //Soma
-        String nomeCompleto =  "Raphael" + "Augusto";
+
+        int multiplicacao = 2*2;
+        int divisao = 1/2;
+        int soma = 1+1;
+        int subtracao = 2-1;
+
+        // ++ Incrementa 1 a unidade / -- Decrementa 1 a unidade
+        // ! operador de negacao / != diferente ! =
+        // ? verdadeiro / : falso
+        // && "e" / || "ou"
+
+        // Exemplo
+        int a = 5;
+        int b = 6;
+
+       String resultado = a==b ? "verdadeiro" : "falso";
+       int numero1 = 1;
+       int numero2 = 1;
+       boolean simNAO = numero1 == numero2;
+       String resultadoNumeros = numero1==numero2 ? "Verdadeiro" : "Falso";
+       System.out.print("numero1 é igual ao número2? " + resultadoNumeros);
     }
 }
