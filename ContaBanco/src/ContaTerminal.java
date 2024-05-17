d4f5034fa8faf1473393bcd86e55130e5a919786
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-17 00:42:53 -0300

    Atualizacao no projeto ContaBanco para o DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static double verificarSaldo(double saldoAtual, double valorSolicitado) {
@@ -14,10 +16,20 @@ public class ContaTerminal {
     }
 
     public static void main(String[] args) {
+            Scanner pegarDadosUsuario = new Scanner(System.in).useLocale(Locale.US);
+
             double saldo = 15;
             double valorSolicitado = 22;
+            double valorNecessario = valorSolicitado - saldo;
+
+            System.out.println( "Saldo Atual: "+ saldo + "\nValor do saldo a ser alterado: ");
+            saldo = pegarDadosUsuario.nextDouble();
+            System.out.println("Valor Solicitado:");
+            valorSolicitado = pegarDadosUsuario.nextDouble();
+
             double saldoAtual = verificarSaldo(saldo, valorSolicitado);
-            System.out.println(saldoAtual);
+            System.out.println("Você precisa de " + valorNecessario + " para realizar essa operação."+
+                    "\nSaldo Atual: " +saldoAtual + ".");
 
     }
 }
