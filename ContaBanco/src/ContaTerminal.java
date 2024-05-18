12346257eb43282d8137b34f09a09b4ac311af4a
raphaelaugustb <devtravassosraphael12@gmail.com>
2024-05-18 20:29:43 -0300

    Melhoras na qualidade do código

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,34 +2,29 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static double verificarSaldo(double saldoAtual, double valorSolicitado) {
+    public static void verificarSaldo(double saldoAtual, double valorSolicitado) {
         if (saldoAtual > valorSolicitado){
             saldoAtual = saldoAtual - valorSolicitado;
-
+            String saldoRealizado = "Saque de: " + valorSolicitado + " reais realizado." + "\nSaldo Atual: " +saldoAtual + " reais.";
+            System.out.println(saldoRealizado);
         }
         else{
-            String saldoErro = "Saldo insuficiente";
+            double valorNecessario = valorSolicitado - saldoAtual;
+            String saldoErro = "Saldo insuficiente: " + "Você precisa de " + valorNecessario + " reais para realizar essa operação. "
+                    + "\nSaldo Atual: " +saldoAtual + " reais.";
             System.out.println(saldoErro);
-
         }
-        return saldoAtual;
     }
 
     public static void main(String[] args) {
             Scanner pegarDadosUsuario = new Scanner(System.in).useLocale(Locale.US);
-
             double saldo = 15;
-            double valorSolicitado = 22;
-            double valorNecessario = valorSolicitado - saldo;
-
-            System.out.println( "Saldo Atual: "+ saldo + "\nValor do saldo a ser alterado: ");
+            double valorSolicitado;
+            System.out.println( "Saldo Atual: "+ saldo + " reais. \nValor do saldo a ser alterado: ");
             saldo = pegarDadosUsuario.nextDouble();
             System.out.println("Valor Solicitado:");
             valorSolicitado = pegarDadosUsuario.nextDouble();
-
-            double saldoAtual = verificarSaldo(saldo, valorSolicitado);
-            System.out.println("Você precisa de " + valorNecessario + " para realizar essa operação."+
-                    "\nSaldo Atual: " +saldoAtual + ".");
+            verificarSaldo(saldo, valorSolicitado);
 
     }
 }
