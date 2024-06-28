dbe389a7767f1f7cdcb29a0ea02f2ed471350f26
raphaelaugustb <travassosraphael12@gmail.com>
2024-06-27 22:42:46 -0300

    ClientService implementation methods Create,Read,Delete,Update

--- clientfindApi/src/main/java/com/leah/clientfindApi/service/ClientService.java
+++ clientfindApi/src/main/java/com/leah/clientfindApi/service/ClientService.java
@@ -1,4 +1,58 @@
 package com.leah.clientfindApi.service;
 
+
+import com.leah.clientfindApi.entity.Address;
+import com.leah.clientfindApi.entity.Client;
+import com.leah.clientfindApi.repository.ClientRepository;
+import com.leah.clientfindApi.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ClientService {
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+    public Iterable<Client> listALLCliente(){
+        return clientRepository.findAll();
+    }
+    public Client getClientById(Long id){
+        if (clientRepository.findById(id).isPresent()){
+            return clientRepository.findById(id).get();
+        } else {
+            throw new RuntimeException("Cliente não está adicionado na base de dados");
+        }
+    }
+    public void changeClientInfo(Long id, Address address, String email){
+        Optional<Client> client = clientRepository.findById(id);
+        if (client.isPresent()){
+            client.get().setAdress(address);
+            client.get().setEmail(email);
+        } else {
+            throw new RuntimeException("Cliente não está adicionado na base de dados");
+        }
+
+    }
+
+    public void insertClient(Client client){
+        long idClient = client.getId();
+        String cep = client.getAdress().getCep();
+        Address clienteAddress =  addressRepository.findById(client.getAdress().getCep()).orElseGet(() -> viaCepService.getAddressByCep(cep));
+        addressRepository.save(clienteAddress);
+        client.setAdress(clienteAddress);
+
+        if (!clientRepository.findById(idClient).isPresent()){
+            clientRepository.save(client);
+        } else {
+            throw new RuntimeException("Cliente já está adicionado na base de dados");
+        }
+    }
+    public void deleteClientById(Long id){
+        clientRepository.deleteById(id);
+    }
 }
